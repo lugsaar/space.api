abe618d9141564dca045e63dd3263722cae992bc
dokuhn <dominik.kuhn90@googlemail.com>
2023-04-09 09:32:50 +0200

    adding first mqtt tests to check_status

--- check_status.py
+++ check_status.py
@@ -8,13 +8,62 @@
 import json
 import os
 import subprocess
+import paho.mqtt.client as mqttc
+
+has_changed = None
+data = None
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+    client.subscribe("space.api/state")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    
+
+    if( msg.topic == "space.api/state"):
+
+        print(msg.topic+" "+str(msg.payload))
+
+        is_open = str(msg.payload)
+
+        global has_changed
+        global data
+        
+        has_changed = data["state"]["open"] != is_open
+        data["state"]["open"] = is_open
+
+        print("Status " + ("changed to " if has_changed else "remains ") + ("open" if is_open else "closed") + ".")
+
+        if has_changed:
+            with open(DATA, "w") as f:
+                json.dump(data, f, indent=4)
+            subprocess.check_call(["git", "add", "api.json"], cwd=HERE)
+            subprocess.check_call(["git", "commit", "-m", "space is " + ("open" if is_open else "closed")], cwd=HERE)
+        subprocess.call(["git", "push"], cwd=HERE)
+
+
+    
+
 
 ## CONSTANTS
 HERE = os.path.dirname(__file__) or "."
 DATA = os.path.join(HERE, "api.json")
 # status.sh - ping
 # status.py - GPIO button press
-STATUS = os.path.join(HERE, "status.py")
+# STATUS = os.path.join(HERE, "status.py")
+
+client = mqttc.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("rohrpostix", 1883, 60)
+
 
 ## ALGORITHM
 subprocess.call(["git", "pull"], cwd=HERE)
@@ -23,16 +72,10 @@ subprocess.call(["git", "pull"], cwd=HERE)
 with open(DATA) as f:
     data = json.load(f)
 
-is_open = subprocess.call([STATUS]) == 0
+# is_open = subprocess.call([STATUS]) == 0
 
-has_changed = data["state"]["open"] != is_open
-data["state"]["open"] = is_open
-
-print("Status " + ("changed to " if has_changed else "remains ") + ("open" if is_open else "closed") + ".")
-
-if has_changed:
-    with open(DATA, "w") as f:
-        json.dump(data, f, indent=4)
-    subprocess.check_call(["git", "add", "api.json"], cwd=HERE)
-    subprocess.check_call(["git", "commit", "-m", "space is " + ("open" if is_open else "closed")], cwd=HERE)
-subprocess.call(["git", "push"], cwd=HERE)
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
